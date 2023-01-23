077aa1e04eb4d4326c5e7385e12323f6d1c743e7
ellananto <ellananto9@gmail.com>
2023-01-23 15:25:00 -0700

    driver is now named

--- Mission3Project/DriverClass.cs
+++ Mission3Project/DriverClass.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Mission3Project
 {
-    class Program
+    class DriverClass
     {
         static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
         static void Main(string[] args)
